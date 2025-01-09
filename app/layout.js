145e37bad9f55d86571d985a2f8cab6b53ecc130
Bodemola <bodemola123@gmail.com>
2025-01-09 09:31:40 +0100

    Hopefully it fixes IOS issue

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,21 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <style>
+          {`
+            /* Fix for iOS background zoom issue */
+            @supports (-webkit-touch-callout: none) {
+              body {
+                background-attachment: scroll; /* Prevent zoom issue on iPhones */
+              }
+            }
+          `}
+        </style>
       </head>
       <body
-        className={`${inter.variable} antialiased scroll-container scrollbar-hide overflow-hidden md:bg-[url('/stars.svg.svg')] sm:bg-sm-bg bg-black w-full h-full bg-cover bg-no-repeat bg-fixed bg-center bg-origin-content`}
+        className={`${inter.variable} antialiased scroll-container scrollbar-hide overflow-hidden 
+        md:bg-[url('/stars.svg.svg')] sm:bg-sm-bg bg-black w-full h-full bg-cover bg-no-repeat 
+        min-h-100vh bg-center bg-origin-border`}
       >
         <StarsCanvas /> {/* Background layer */}
         <Animate /> {/* Swirling images above snow */}
