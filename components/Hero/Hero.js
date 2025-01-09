bc8ed7bb515bf3af53418df186c7c6ef68509b82
Bodemola123 <112621700+Bodemola123@users.noreply.github.com>
2025-01-09 16:26:13 +0100

    Update Hero.js

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -14,19 +14,19 @@ const Hero = () => {
 
   return (
     <div
-      className="flex flex-col items-center justify-center text-center h-screen bg-cover bg-center bg-no-repeat relative gap-[21px] z-15"
+      className="flex flex-col items-center justify-center text-center h-screen bg-cover bg-center bg-no-repeat relative gap-[21px] z-20"
       style={{ backgroundImage: "url('/background.png')" }}
     >
       <div className="flex flex-row justify-center items-center">
         <div className="lg:h-[322px] md:h-[198px] sm:h-[118px] mx-auto">
-          <h1 className="lg:text-[165px] md:text-[98.67px] sm:text-[58.88px] sm:font-bold md:font-bold lg:font-black leading-tight text-[#ffffff] z-15">
+          <h1 className="lg:text-[165px] md:text-[98.67px] sm:text-[58.88px] sm:font-bold md:font-bold lg:font-black leading-tight text-[#ffffff] z-20">
             Ideas Into Workflow
           </h1>
         </div>
       </div>
 
       <p
-        className={`flex justify-center items-center md:mt-12 lg:mt-12 sm:mt-[5.97px] lg:text-xl md:text-base sm:text-base text-[#ffffff] md:leading-[25.6px] sm:leading-[23.92px] lg:w-[977px] z-15 text-center md:w-[584.27px] sm:w-auto sm:px-10 md:px-0 transition-opacity duration-1000 ${
+        className={`flex justify-center items-center md:mt-12 lg:mt-12 sm:mt-[5.97px] lg:text-xl md:text-base sm:text-base text-[#ffffff] md:leading-[25.6px] sm:leading-[23.92px] lg:w-[977px] z-20 text-center md:w-[584.27px] sm:w-auto sm:px-10 md:px-0 transition-opacity duration-1000 ${
           isVisible ? "opacity-100" : "opacity-0"
         }`}
       >
@@ -35,7 +35,7 @@ const Hero = () => {
 
       <div className="flex md:flex-row lg:flex-row sm:flex-col sm:gap-[18px] lg:gap-4 md:gap-[15.57px] mt-3 justify-center items-center">
         <button
-          className="bg-[#c088fb] flex items-center justify-center sm:py-3 md:py-2.5 sm:px-3 md:px-9 rounded-3xl md:gap-2.5 lg:gap-2.5 sm:gap-[5.98px] text-[#141414] font-medium md:text-base sm:text-base transition-all duration-200 ease-in-out hover:scale-110 hover:bg-[#9b67d7] md:w-auto sm:w-[286px] z-15"
+          className="bg-[#c088fb] flex items-center justify-center sm:py-3 md:py-2.5 sm:px-3 md:px-9 rounded-3xl md:gap-2.5 lg:gap-2.5 sm:gap-[5.98px] text-[#141414] font-medium md:text-base sm:text-base transition-all duration-200 ease-in-out hover:scale-110 hover:bg-[#9b67d7] md:w-auto sm:w-[286px] z-20"
         >
           Start your AI journey
         </button>
