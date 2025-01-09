19677e44644eedcdf7bbaa2fc540a0357a995d27
Bodemola <bodemola123@gmail.com>
2025-01-09 09:53:24 +0100

    tried fixing the effect on Iphone

--- app/layout.js
+++ app/layout.js
@@ -11,29 +11,27 @@ const inter = Inter({
 
 export const metadata = {
   title: "Voyex",
-  description: "Voyex is the ultimate superapp for discovering AI tools and building custom workflows tailored to your needs. Effortlessly find, use, and query the right tools to create your own mini agency by simply describing your use case.",
+  description:
+    "Voyex is the ultimate superapp for discovering AI tools and building custom workflows tailored to your needs. Effortlessly find, use, and query the right tools to create your own mini agency by simply describing your use case.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <style>
-          {`
-            /
-            @supports (-webkit-touch-callout: none) {
-              body {
-                background-attachment: scroll; /* Prevent zoom issue on iPhones */
-              }
-            }
-          `}
-        </style>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </head>
       <body
-        className={`${inter.variable} antialiased scroll-container scrollbar-hide overflow-hidden 
-        md:bg-[url('/stars.svg.svg')] sm:bg-sm-bg bg-black w-full h-full bg-cover bg-no-repeat 
-       bg-center bg-origin-content bg-fixed`}
+        className={`${inter.variable} antialiased scroll-container scrollbar-hide overflow-hidden md:bg-[url('/stars.svg')] sm:bg-sm-bg bg-black w-full h-full bg-cover bg-no-repeat bg-center bg-origin-content`}
+        style={{
+          backgroundAttachment:
+            typeof window !== "undefined" && /iPhone|iPad|iPod/.test(navigator.userAgent)
+              ? "scroll"
+              : "fixed", // Apply background-attachment: scroll for iOS
+        }}
       >
         <StarsCanvas /> {/* Background layer */}
         <Animate /> {/* Swirling images above snow */}
