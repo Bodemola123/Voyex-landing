85879e8fccf084a72e67a8abec60ab3132f7ecc8
Bodemola <bodemola123@gmail.com>
2025-02-27 13:30:44 +0100

    Now to move on the blog

--- components/contact-us-page/Hero.js
+++ components/contact-us-page/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex lg:px-24 sm:px-14 lg:mt-36 sm:mt-28 h-full w-full items-center justify-start'>
+    <div className='flex lg:px-32 md:px-16 sm:px-10 lg:mt-36 sm:mt-28 h-full w-full items-center justify-start'>
         <div className='flex flex-col lg:gap-5 sm:gap-3 items-start justify-start text-[#f4f4f4]'>
             <div className='flex flex-col lg:gap-3 sm:gap-2 text-left'>
                 <p className='font-semibold lg:text-base sm:text-sm'>Contact us</p>
