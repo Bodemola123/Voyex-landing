c2726a6ecb82da9fa434bc3740c01658b4431a0c
Nisarga.gowda <158475422+NisargaHGowda@users.noreply.github.com>
2025-04-16 15:19:54 +0530

    updated

--- app/blog/page.js
+++ app/blog/page.js
@@ -3,6 +3,7 @@
 import Final from '@/components/about-us-page/Final'
 import Blog from '@/components/blog-page/Blog'
 import Combo from '@/components/blog-page/Combo'
+import CTA from '@/components/blog-page/CTA'
 import React from 'react'
 
 const page = () => {
@@ -12,7 +13,7 @@ const page = () => {
       <Blog/>
       <div className='relative px-9 lg:w-[786px] lg:h-[270px] md:w-[478.15px] md:h-[110px] sm:w-[226.29px] sm:h-[90px] lg:mt-[370px] md:mt-[440px] sm:mt-[620px] top-[40px] opacity-100 rounded-b-full mx-auto'>
 </div>
-     
+        <CTA/>
         <Final/>
     </div>
   )
