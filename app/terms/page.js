f6a1847860f343083ab92aaad7e881a1133738b0
Nisarga.gowda <158475422+NisargaHGowda@users.noreply.github.com>
2025-04-17 23:05:58 +0530

    updated

--- app/terms/page.js
+++ app/terms/page.js
@@ -1,5 +1,12 @@
+"use client"
 import Terms from "@/components/privacy-terms-page/terms";
+import Final from "@/components/privacy-terms-page/Final";
 
 export default function TermsPage() {
-  return <Terms />;
+  return (
+  <div className='relative flex flex-col w-full h-full'>
+    <Terms />
+  <Final/>
+  </div>
+  )
 }
